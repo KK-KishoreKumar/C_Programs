644ac088a6a5f1b1060cb01c2cf3ecec59cb1ac6
kk <kklinuxkernel@gmail.com>
2018-02-02 11:48:36 +0530

    historgram_program
    DESC: Graphical Display using C
    
    Signed off by: Kishore.Kumar <kklinuxkernel@gmail.com>

---
+++ historgram.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAXv 10
+#define MAXh 20
+
+int main(int argc, char* argv[])
+{
+
+	int arr[MAXh] = {0};
+	int i, j;
+	int count = 0;
+	int c;
+
+	while ((c = getchar()) != EOF) {
+
+		if (c != ' ') {
+
+			count++;
+		} else {
+			++arr[count];
+			count = 0;
+		}
+
+	}	
+	
+		for(i = 0; i < 20; i++)
+			printf(" %d", arr[i]);
+
+		printf("\n");
+
+	for (i = MAXv; i >= 1; i--) {
+	
+		printf(" %3d | ",i);
+		for (j = 1; j <= MAXh; j++) {
+		
+			if (i == arr[j]) {
+				printf("*");
+				printf("  ");
+			} else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+
+	}
+
+	printf("\n");
+
+
+	printf("\n");
+
+	printf("       ");
+	printf(".................................................\n");
+	printf("       ");
+	for (i = 1; i <= MAXh; i++) {
+
+		printf("%d", i);
+		printf("  ");
+	}
+	printf("\n");
+	return 0;
+}
